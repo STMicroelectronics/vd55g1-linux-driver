78b0b38a2a803e55386a0b500dbe13f65aadee97
Benjamin Mugnier <benjamin.mugnier@foss.st.com>
2024-10-24 10:25:37 +0200

    Apply format unconditionally
    Signed-off-by: Benjamin Mugnier <benjamin.mugnier@foss.st.com>

--- vd55g1.c
+++ vd55g1.c
@@ -1444,13 +1444,6 @@ static int vd55g1_set_pad_fmt(struct v4l2_subdev *sd,
 	format = v4l2_subdev_state_get_format(sd_state, sd_fmt->pad);
 #endif
 
-	/* Avoid to reset ctrls while format hasn't changed */
-	if (sd_fmt->format.width == format->width &&
-	    sd_fmt->format.height == format->height &&
-	    sd_fmt->format.code == format->code &&
-	    sd_fmt->which == V4L2_SUBDEV_FORMAT_ACTIVE)
-		return 0;
-
 	/* Update active state's format and crop */
 	if (sd_fmt->which == V4L2_SUBDEV_FORMAT_ACTIVE)
 		vd55g1_new_format_change_controls(sensor);
